455dc9c8c63588adad3f40b4fa4e69c7e3b40210
GamingKoLla <86671054+GamingKoLla@users.noreply.github.com>
2024-02-10 19:10:18 +0530

    config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 module.exports = {
 	helpCmdPerPage: 10, //- Number of commands per page of help command
 	lyricsMaxResults: 5, //- Number of results for lyrics command (Do not touch this value if you don't know what you are doing)
-	adminId: "UserId", //- Replace UserId with the Discord ID of the admin of the bot
-	token: process.env.token || "", //- Bot's Token
-	clientId: process.env.clientId || "", //- ID of the bot
-	clientSecret: process.env.clientSecret || "", //- Client Secret of the bot
+	adminId: "856861743024308234", //- Replace UserId with the Discord ID of the admin of the bot
+	token: process.env.token || "MTE3MzY2MjExOTIwNDUwNzc2OQ.GEM6s6.JboUWQtMeIzGRp2EbvQ8t_J9was6GaSJlaDbg4", //- Bot's Token
+	clientId: process.env.clientId || "1173662119204507769", //- ID of the bot
+	clientSecret: process.env.clientSecret || "6TUdrmqpwrpuBUSLsZPDuJdmkEbwIj0h", //- Client Secret of the bot
 	port: 4200, //- Port of the API and Dashboard
 	scopes: ["identify", "guilds", "applications.commands"], //- Discord OAuth2 Scopes
 	inviteScopes: ["bot", "applications.commands"], // Invite link scopes
 	serverDeafen: true, //- If you want bot to stay deafened
 	defaultVolume: 100, //- Sets the default volume of the bot, You can change this number anywhere from 1 to 100
-	supportServer: "https://discord.gg/sbySMS7m3v", //- Support Server Link
-	Issues: "https://github.com/SudhanPlayz/Discord-MusicBot/issues", //- Bug Report Link
+	supportServer: "", //- Support Server Link
+	Issues: "", //- Bug Report Link
 	permissions: 277083450689, //- Bot Inviting Permissions
 	disconnectTime: 30000, //- How long should the bot wait before disconnecting from the voice channel (in miliseconds). Set to 1 for instant disconnect.
 	twentyFourSeven: false, //- When set to true, the bot will never disconnect from the voice channel
